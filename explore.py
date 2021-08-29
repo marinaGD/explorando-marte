5baf7a9a8b3028d284e7b2d5606cc31bdee77fcf
Marina Domingues <mgd.marinadomingues@gmail.com>
2021-08-29 18:32:15 -0300

    Implementa a utilização da classe Explorer

--- explore.py
+++ explore.py
@@ -18,9 +18,9 @@ def get_mission_info(file_path: str) -> Tuple[Tuple[int, int], List[dict]]:
     with open(file_path) as file_:
         file_data = file_.read().splitlines()
     rect_x, rect_y = file_data[0].split()
-    coordinate = (int(rect_x), int(rect_y))
-    explorers = make_explorers(file_data[1:])
-    return coordinate, explorers
+    Explorer.limit_x = int(rect_x)
+    Explorer.limit_y = int(rect_y)
+    return make_explorers(file_data[1:])
 
 
 def make_explorers(explorers_data: List[str]) -> List[dict]:
@@ -38,12 +38,14 @@ def make_explorers(explorers_data: List[str]) -> List[dict]:
     explorers = []
     for index in range(0, len(explorers_data), 2):
         explorer_init = explorers_data[index].split()
-        explorer_info = {
-            'ini_pos': (int(explorer_init[0]), int(explorer_init[1])),
-            'att': explorer_init[2],
-            'seq_mov': explorers_data[index+1]
-        }
-        explorers.append(explorer_info)
+        explorer = Explorer(
+            int(explorer_init[0]), int(explorer_init[1]),
+            explorer_init[2],
+            explorers_data[index+1])
+        if explorer is not None:
+            explorers.append(explorer)
+        else:
+            print(f'Sonda #{index//2+1} ignorada. Parâmetros inválidos.')
     return explorers
 
 
